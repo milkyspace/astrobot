5b25554d8a2134d4f17d20e31ce846440bb1c674
Владимир Колеснев <you@example.com>
2025-12-28 22:25:11 +0700

    init

--- bot/bot.py
+++ bot/bot.py
@@ -7,6 +7,7 @@ from aiogram.client.default import DefaultBotProperties
 
 from bot.handlers.start import router as start_router
 from bot.handlers.menu import router as menu_router
+from bot.handlers.main_menu import router as to_main_menu
 from bot.handlers.natal import router as natal_router
 from bot.handlers.karma import router as karma_router
 from bot.handlers.solar import router as solar_router
@@ -28,6 +29,7 @@ async def main():
     # регистрируем хендлеры
     dp.include_router(start_router)
     dp.include_router(menu_router)
+    dp.include_router(to_main_menu)
     dp.include_router(natal_router)
     dp.include_router(karma_router)
     dp.include_router(solar_router)
