e68532c1ca6d86716e392716be87226df2a32b22
Владимир Колеснев <you@example.com>
2025-12-29 10:30:43 +0700

    init

--- worker/telegram.py
+++ worker/telegram.py
@@ -1,9 +1,9 @@
-# worker/telegram.py
-import os
 import requests
+import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-BASE = f"https://api.telegram.org/bot{TOKEN}"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BASE = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
 
 def edit_message(chat_id: int, message_id: int, text: str):
     requests.post(
@@ -12,6 +12,8 @@ def edit_message(chat_id: int, message_id: int, text: str):
             "chat_id": chat_id,
             "message_id": message_id,
             "text": text,
+            "parse_mode": "HTML",
+            "disable_web_page_preview": True,
         },
         timeout=10,
     )
